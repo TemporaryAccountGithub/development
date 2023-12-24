855c213728203cd18cd8fe1e1fcc4e5ea387f242
TemporaryAccountGithub <tmpanonimi@gmail.com>
2023-12-24 16:03:02 +0200

    test4 fix

--- TDD/Calculator/Calculator.cs
+++ TDD/Calculator/Calculator.cs
@@ -9,6 +9,11 @@
                 throw new OverflowException("Sum causes double overflow!");
             }
 
+            if(firstNum < 0 && secondNum < double.MinValue - firstNum)
+            {
+                throw new OverflowException("Sum causes double underflow!");
+            }
+
             return firstNum + secondNum;
         }
     }
