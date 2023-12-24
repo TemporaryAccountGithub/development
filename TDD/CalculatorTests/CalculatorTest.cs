726cfdef74f51b6f0480f36790206a1652d940db
TemporaryAccountGithub <tmpanonimi@gmail.com>
2023-12-24 15:47:28 +0200

    refactor test2

--- TDD/CalculatorTests/CalculatorTest.cs
+++ TDD/CalculatorTests/CalculatorTest.cs
@@ -15,6 +15,8 @@ namespace CalculatorTests
         public void given_numbers_when_add_then_returnSum()
         {
             Assert.AreEqual(Calculator.Calculate(1, 2, '+'), 3);
+            Assert.AreEqual(Calculator.Calculate(-1, -2, '+'), -3);
+            Assert.AreEqual(Calculator.Calculate(-1, 2.5, '+'), 1.5);
         }
     }
 }
