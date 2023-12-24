0ca95d4c455b4e2b9c02ba6d3e4f00d65b539437
TemporaryAccountGithub <tmpanonimi@gmail.com>
2023-12-24 15:55:35 +0200

    test3 fail

--- TDD/CalculatorTests/CalculatorTest.cs
+++ TDD/CalculatorTests/CalculatorTest.cs
@@ -18,5 +18,11 @@ namespace CalculatorTests
             Assert.AreEqual(Calculator.Calculate(-1, -2, '+'), -3);
             Assert.AreEqual(Calculator.Calculate(-1, 2.5, '+'), 1.5);
         }
+
+        [TestMethod]
+        public void given_numbersOverflow_when_add_then_throwException()
+        {
+            Assert.ThrowsException<Exception>(() => Calculator.Calculate(double.MaxValue, 0.1, '+'));
+        }
     }
 }
