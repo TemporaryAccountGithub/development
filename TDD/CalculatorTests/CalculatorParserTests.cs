5e73768e31b88806e068213067599a1791f8a4de
TemporaryAccountGithub <tmpanonimi@gmail.com>
2023-12-27 20:20:43 +0200

    creating parser tester

---
+++ TDD/CalculatorTests/CalculatorParserTests.cs
@@ -0,0 +1,34 @@
+﻿using CalculatorLibrary;
+using Moq;
+
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorParserTests
+    {
+        ICalculatorParser parser;
+        List<double> expectedNumbers;
+        List<char> expectedOperations;
+        string expression;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            parser = new CalculatorParser();
+        }
+
+        [TestMethod]
+        public void given_string_when_parse_then_returnLists()
+        {
+            expression = "1+2";
+            expectedNumbers = new List<double>{ 1, 2 };
+            expectedOperations = new List<char> { '+' };
+
+            var result = parser.ParseExpression(expression);
+
+            CollectionAssert.AreEqual(result.Item1, expectedNumbers);
+            CollectionAssert.AreEqual(result.Item2, expectedOperations);
+        }
+    }
+}
