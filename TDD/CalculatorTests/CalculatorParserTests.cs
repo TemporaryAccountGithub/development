3f345a99cf99c0dc3b127f3338f1c733cbcd88fb
TemporaryAccountGithub <tmpanonimi@gmail.com>
2023-12-28 08:57:19 +0200

    adding parser ^& tests

--- TDD/CalculatorTests/CalculatorParserTests.cs
+++ TDD/CalculatorTests/CalculatorParserTests.cs
@@ -30,5 +30,31 @@ namespace CalculatorTests
             CollectionAssert.AreEqual(result.Item1, expectedNumbers);
             CollectionAssert.AreEqual(result.Item2, expectedOperations);
         }
+
+        [TestMethod]
+        public void given_stringWithPower_when_parse_then_returnLists()
+        {
+            expression = "1^2";
+            expectedNumbers = new List<double> { 1, 2 };
+            expectedOperations = new List<char> { '^' };
+
+            var result = parser.ParseExpression(expression);
+
+            CollectionAssert.AreEqual(result.Item1, expectedNumbers);
+            CollectionAssert.AreEqual(result.Item2, expectedOperations);
+        }
+
+        [TestMethod]
+        public void given_stringWithSqrt_when_parse_then_returnLists()
+        {
+            expression = "&5";
+            expectedNumbers = new List<double> { 0, 5 };
+            expectedOperations = new List<char> { '&' };
+
+            var result = parser.ParseExpression(expression);
+
+            CollectionAssert.AreEqual(result.Item1, expectedNumbers);
+            CollectionAssert.AreEqual(result.Item2, expectedOperations);
+        }
     }
 }
